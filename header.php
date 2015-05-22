ad178ad12612392cd9325a01bc807f3e9ac4b971
Arco Overbeek <arco@web-mine.nl>
2015-05-22 12:37:45 +0200

    Update JS

--- header.php
+++ header.php
@@ -9,7 +9,8 @@
     <title>Dit komt in je browser window</title>
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/main.css">
-    <script src="js/main.js"></script>
+    <script src="javascript/jquery.js"></script>
+    <script src="javascript/main.js"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
   </head>
   <body>
@@ -27,26 +28,17 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li class="active"><a href="/reservation_app">Home</a></li>
             <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Kamers <span class="caret"></span></a>
               <ul class="dropdown-menu" role="menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
+                <li><a href="rooms">Alle kamers</a></li>
+                <li><a href="/rooms/new.php">Nieuwe kamer</a></li>
               </ul>
             </li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
             <li><a href="../navbar/">Default</a></li>
-            <li class="active"><a href="./">Static top <span class="sr-only">(current)</span></a></li>
-            <li><a href="../navbar-fixed-top/">Fixed top</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
--- rooms/index.php
+++ rooms/index.php
@@ -1,8 +1,8 @@
 <?php
   include '../header.php';
 ?>
-  <h1>Nieuwe kamer</h1>
+<h1 class="page-header">Alle Kamers</h1>
+
 <?php
-  include 'form.php';
   include '../footer.php';
 ?>
